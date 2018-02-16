aebe22491118e062a755a539b1ad9a4b822e78bd
Marshall Asch <maasch@rogers.com>
2018-02-16 14:58:14 -0500

    added support for specifyung ether gitlab or github, still keeps the legacy support for the old endpoint. planning on adding support for different scripts for different event types

--- app.js
+++ app.js
@@ -19,6 +19,74 @@ app.use(express.static(__dirname));
 app.use(bodyParser.json());
 
 
+// This is for github web hooks
+app.post('/v1/github', function(req, res) {
+
+	var id = req.body.repository.id;
+	var ref = req.body.ref;
+	var foundServer = 0;
+
+	for (var i = 0; i < configuration.servers.length; i++)
+	{
+		console.log(id + " " + configuration.servers[i].repositoryID);
+		if (configuration.servers[i].repositoryID == id && configuration.servers[i].ref == ref) {
+			child = exec(configuration.servers[i].runScript, function (error, stdout, stderr) {
+				console.log('stdout: ' + stdout);
+				console.log('stderr: ' + stderr);
+		 		if (error !== null) {
+					console.log('exec error: ' + error);
+				}
+			});
+
+			res.status(200);
+			res.send(JSON.stringify({"status": 200}));
+			foundServer = 1;
+
+			break;
+		}
+	}
+
+	if (foundServer === 0) {
+		res.status(404);
+		res.send(JSON.stringify({"status": 404, "message": "Rule for repository \"" + req.body.repository.name + "\" (" + id + ") not found"}));
+	}
+});
+
+
+// this is for gitlab webhooks
+app.post('/v1/gitlab', function(req, res) {
+
+	var id = req.body.project.id;
+	var ref = req.body.ref;
+	var foundServer = 0;
+
+	for (var i = 0; i < configuration.servers.length; i++)
+	{
+		console.log(id + " " + configuration.servers[i].repositoryID);
+		if (configuration.servers[i].repositoryID == id && configuration.servers[i].ref == ref) {
+			child = exec(configuration.servers[i].runScript, function (error, stdout, stderr) {
+				console.log('stdout: ' + stdout);
+				console.log('stderr: ' + stderr);
+		 		if (error !== null) {
+					console.log('exec error: ' + error);
+				}
+			});
+
+			res.status(200);
+			res.send(JSON.stringify({"status": 200}));
+			foundServer = 1;
+
+			break;
+		}
+	}
+
+	if (foundServer === 0) {
+		res.status(404);
+		res.send(JSON.stringify({"status": 404, "message": "Rule for repository \"" + req.body.repository.name + "\" (" + id + ") not found"}));
+	}
+});
+
+
 app.post('/git-master-update', function(req, res) {
 
 	var id = req.body.repository.id;
