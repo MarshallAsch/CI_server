cb8e6387467ec6b7c0a45f7fbd058b57ba890143
Marshall Asch <maasch@rogers.com>
2019-08-11 15:12:08 -0700

    escape chatacters that need to be escaped before passing it to bash

--- app.js
+++ app.js
@@ -26,7 +26,14 @@ function runScript(script, eventType, event, argument="") {
 		done: false,
 	};
 
-	const child = exec(`${script} "${eventType}" "${JSON.stringify(event).replace(/"/gi, '\\\"')}"  ${argument}`  , function (error, stdout, stderr) {
+	// escape characters that need to be escaped
+	let eventStr = JSON.stringify(event);
+	eventStr = eventStr.replace(/\\/gi, '\\\\');
+	eventStr = eventStr.replace(/"/gi, '\\\"');
+	eventStr = eventStr.replace(/\$/gi, '\\$');
+
+
+	const child = exec(`${script} "${eventType}" "${eventStr}"  ${argument}`  , function (error, stdout, stderr) {
 		console.log('stdout: ' + stdout);
 		console.log('stderr: ' + stderr);
 
